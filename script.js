f11050c193a0512b6707f55a454fab5c7bb84ead
ShadowVectorZ <rayaudet19@gmail.com>
2024-07-09 12:22:49 -0400

    added code that creates new todos and pushes them to current project array
    also fixed code that enables todos to be seen
    on dom

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-function makeNewTodo(name){
-    let title=name
-    let description="description"
-    let dueDate="dueDate"
-    let priority="priority"
+function makeNewTodo(title,description,dueDate,priority){
+     title=title
+     description=description
+    dueDate=dueDate
+    priority=priority
 
-
-
-    
     return{title,description,dueDate,priority}
 }
 
+let h=makeNewTodo('h','i','j','k')
+let i=makeNewTodo('h','i','j','k')
 
-function addTodoToProject(){
-    let newTodo
-    projects[currentProject].push(newTodo)
+
+function addTodoToProject(title,description,dueDate,priority){
+    let newTodo=makeNewTodo(title,description,dueDate,priority)
+    makeProject.currentProject.push(newTodo)
 }
 
 let makeProject=(function(project){
-    let projects=[]
+    let defaultProject=[]
+    let currentProject=defaultProject
     let makeNewProject=function(){
-         project=['']
-        projects.push(project)
-
+         project=[]
     }
-    return{projects,makeNewProject}
+
+   
+    return{defaultProject,currentProject,project,makeNewProject,}
+})()
+
+makeProject.currentProject.push(h,i)
+
+let renderTasks=(function(){
+
+    let printList=function(){
+        document.querySelector('#container')
+        let array=makeProject.currentProject;
+        for(let j=0;j<array.length;j++){
+            const content=document.createElement('div')
+            content.classList.add('content')
+            content.textContent=`${makeProject.currentProject[j].title}`
+            container.appendChild(content)
+        }
+    }
+
+
+    return{printList,}
+
 })()
